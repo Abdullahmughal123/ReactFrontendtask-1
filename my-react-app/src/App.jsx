3f9d0958199eb2cd63927a8744374fdb6f0a1d37
Abdullahmughal123 <146ammughal@gmail.com>
2026-02-24 15:24:00 +0500

    Added Counter component using useState

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,27 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import UserCard from "./components/UserCard";
+import Counter from "./components/Counter";
 
+// function App() {
+//   return (
+//     <>
+//       <Header title="My Team Members" />
+
+//       <UserCard name="Abdullah Shahid" role="React Intern" city="Lahore" />
+//       <UserCard name="Ali Khan" role="Backend Developer" city="Karachi" />
+//       <UserCard name="Sara Ahmed" role="UI/UX Designer" city="Islamabad" />
+
+//       <Footer text="© 2026 My React Learning Project" />
+//     </>
+//   );
+// }
 function App() {
   return (
     <>
-      <Header title="My Team Members" />
-
-      <UserCard name="Abdullah Shahid" role="React Intern" city="Lahore" />
-      <UserCard name="Ali Khan" role="Backend Developer" city="Karachi" />
-      <UserCard name="Sara Ahmed" role="UI/UX Designer" city="Islamabad" />
-
-      <Footer text="© 2026 My React Learning Project" />
+      <h1>React useState Practice</h1>
+      <Counter />
     </>
   );
 }
-
 export default App;
